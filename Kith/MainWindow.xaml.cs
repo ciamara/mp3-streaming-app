1f4f423a51cb81800b56059f4c2462e5b7b7b8b6
ciamara <mruktamara64@gmail.com>
2025-07-13 14:09:57 +0200

    updated gitignore

---
+++ Kith/MainWindow.xaml.cs
@@ -0,0 +1,69 @@
+using Microsoft.UI.Windowing;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+
+
+namespace Kith
+{
+    public sealed partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            this.InitializeComponent();
+
+            AppWindow.SetIcon("Assets/Tiles/GalleryIcon.ico");
+            AppWindow.TitleBar.PreferredTheme = TitleBarTheme.UseDefaultAppMode;
+            OverlappedPresenter presenter = OverlappedPresenter.Create();
+
+            presenter.IsAlwaysOnTop = false;
+            presenter.IsMaximizable = true;
+            presenter.IsMinimizable = true;
+            presenter.IsResizable = true;
+            presenter.SetBorderAndTitleBar(false, false);
+
+            AppWindow.SetPresenter(presenter);
+
+            SizeChanged += MainWindow_SizeChanged;
+        }
+        private void MaximizeRestoreBtn_Click(object sender, RoutedEventArgs e)
+        {
+            OverlappedPresenter presenter = (OverlappedPresenter)AppWindow.Presenter;
+            if (presenter.State == OverlappedPresenterState.Maximized)
+            {
+                presenter.Restore();
+            }
+            else
+            {
+                presenter.Maximize();
+            }
+        }
+        private void MainWindow_SizeChanged(object sender, WindowSizeChangedEventArgs e)
+        {
+            OverlappedPresenter presenter = (OverlappedPresenter)AppWindow.Presenter;
+            MaximizeRestoreBtn.Content = presenter.State == OverlappedPresenterState.Maximized ? "Restore" : "Maximize";
+        }
+        private void MinimizeBtn_Click(object sender, RoutedEventArgs e)
+        {
+            OverlappedPresenter presenter = (OverlappedPresenter)AppWindow.Presenter;
+            presenter.Minimize();
+        }
+        private void CloseBtn_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+    }
+}
