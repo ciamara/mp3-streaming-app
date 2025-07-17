88700dc94ae8a79126e13ea7cc99724d379c4069
ciamara <mruktamara64@gmail.com>
2025-07-17 19:46:26 +0200

    Complete sections with gradients;resizable window responsive with minsize;draggable window

--- Kith/MainWindow.xaml.cs
+++ Kith/MainWindow.xaml.cs
@@ -1,3 +1,4 @@
+using Kith.Sources;
 using Microsoft.UI;
 using Microsoft.UI.Windowing;
 using Microsoft.UI.Xaml;
@@ -24,6 +25,12 @@ namespace Kith
         public MainWindow()
         {
             this.InitializeComponent();
+            //setting topsection of grid to be titlebar (in order to be draggable)
+            CustomizeWindow();
+
+            //setting min window size
+            var hwnd = WinRT.Interop.WindowNative.GetWindowHandle(this);
+            _ = new WindowsSubclass(hwnd, 69, new Size(930, 550));
 
             AppWindow.SetIcon("Assets/Tiles/GalleryIcon.ico");
             AppWindow.TitleBar.PreferredTheme = TitleBarTheme.UseDefaultAppMode;
@@ -38,6 +45,17 @@ namespace Kith
             AppWindow.SetPresenter(presenter);
 
             SizeChanged += MainWindow_SizeChanged;
+        }
+        private void CustomizeWindow()
+        {
+            if (AppWindow?.Presenter is not OverlappedPresenter presenter)
+            {
+                return;
+            }
+
+            presenter.SetBorderAndTitleBar(hasBorder: false, hasTitleBar: false);
+            ExtendsContentIntoTitleBar = true;
+            SetTitleBar(LayoutRoot);
         }
         private void MaximizeRestoreBtn_Click(object sender, RoutedEventArgs e)
         {
@@ -86,6 +104,7 @@ namespace Kith
         {
 
         }
+
 
 
 
---
+++ Kith/Sources/Song.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kith.Sources
+{
+    public class Song
+    {
+        public string Title { get; private set; }
+        public string Artist { get; private set; }
+        public string Album { get; private set; }
+        public double Length { get; private set; }
+
+        public Song(string title, string artist, string album, double length)
+        {
+            Title = title;
+            Artist = artist;
+            Album = album;
+            Length = length;
+        }
+    }
+}
