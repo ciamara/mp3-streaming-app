85541cba312b0f6744833fa746f2c927e79db58b
ciamara <mruktamara64@gmail.com>
2025-12-12 12:21:51 +0100

    customised playbar

---
+++ Kith/Converters/DurationConverter.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.UI.Xaml.Data;
+using System;
+using Windows.UI.Xaml.Data;
+
+namespace Kith.Converters
+{
+    public class DurationConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (value is TimeSpan time)
+            {
+                return time.ToString(@"mm\:ss");
+            }
+            return "00:00";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Kith/Sources/Collection.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TagLib;
+using Windows.UI.Core;
+
+namespace Kith.Sources
+{
+    class Collection
+    {
+        private String collection_name { get; set; }
+        private Bitmap collection_cover { get; set; }
+        private String collection_description { get; set; }
+        private Double collection_duration { get; set; }
+        private uint collection_size { get; set; }
+        private List<Song> collection_songs { get; set; }
+
+        public Collection()
+        {
+            this.collection_name = "your playlist";
+            this.collection_cover = new Bitmap("ms - appx:///Assets/albumplaceholder.png", true);
+            this.collection_description = "your playlist description";
+            this.collection_duration = 0.0;
+            this.collection_size = 0;
+
+            this.collection_songs = new List<Song>();
+        }
+        public void Add(Song song)
+        {
+            collection_songs.Add(song);
+        }
+        public void addSongs(List<Song> songs)
+        {
+            foreach (Song song in songs)
+            {
+                this.collection_songs.Add(song);
+            }
+        }
+        public void Print()
+        {
+            int index = 0;
+            System.Console.WriteLine("Queue:");
+            foreach (Song song in collection_songs)
+            {
+                System.Console.Write(index++ + " ");
+                System.Console.WriteLine(song.Title);
+            }
+        }
+
+        public void ExtractCoverColors()
+        {
+
+        }
+
+    }
+}
